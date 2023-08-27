2d8a0c5b1e7e926b2ec4b7d2dec0e0c1001f0b7f
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-08-27 20:47:07 +0200

    VTT-4 Initialize implementation swiper

---
+++ src/components/pages/home/carousel/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import LayoutContainer from '@/components/layouts/Container/index.vue';
+
+export default defineComponent({
+  name: 'HomeCarousel',
+  components: {
+    LayoutContainer,
+  },
+  setup() {
+    return {}
+  }
+});
+</script>
+<template>
+  <layout-container class="container">
+    <h1>title</h1>
+  </layout-container>
+</template>
+<style lang="scss" scoped>
+.container {
+  padding: 5em 0;
+}
+</style>
