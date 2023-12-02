42888402d424919d4cb27aca2947c4ead0660c0a
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2023-12-02 14:48:56 +0100

    VTT-5 Initialize carousel component

---
+++ src/components/layouts/Carousel/index.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  },
+};
+</script>
+
+<template>
+  <swiper
+      :slides-per-view="3.9"
+      :space-between="45"
+      :centered-slides=true
+      :loop=true
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    <swiper-slide>Slide 7</swiper-slide>
+  </swiper>
+</template>
+
+<style lang="scss" scoped>
+.swiper {
+  .swiper-wrapper {
+    .swiper-slide {
+      min-height: 30em;
+      background-color: blue;
+    }
+  }
+}
+</style>
+
---
+++ src/components/pages/home/carousel/index.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LayoutContainer from '@/components/layouts/Container/index.vue';
+import LayoutCarousel from '@/components/layouts/Carousel/index.vue';
+
+export default defineComponent({
+  components: {
+    LayoutContainer,
+    LayoutCarousel,
+  },
+  setup() {
+    return {}
+  }
+});
+</script>
+
+<template>
+  <layout-container class="container">
+    <div class='inner-headline'>
+      <h3 class='headline'>Our Works</h3>
+      <span class="subheadline">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</span>
+    </div>
+  </layout-container>
+  <layout-carousel />
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin: 3em 0;
+
+  .inner-headline {
+
+    .headline {
+      font-weight: 700;
+      font-size: 3em;
+      color: #1f1f25;
+      margin: 0;
+    }
+
+    .subheadline {
+      display: block;
+      font-weight: 300;
+      color: #717173;
+      font-size: 1.15em;
+      margin-top: 1em;
+    }
+  }
+}
+</style>
